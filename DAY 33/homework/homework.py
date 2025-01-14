8d5d3ec1507b6c246a10d6a655bf7a5511cdbc93
achiko12629 <20achi10@gmail.com>
2025-01-14 10:29:29 +0400

    commit

---
+++ DAY 33/homework/homework.py
@@ -0,0 +1,44 @@
+
+
+
+
+#1
+
+def idk (num1):
+    return num1 * num1
+
+omg = int(input("enter a nomber :"))
+
+print(idk(omg))
+
+
+#2
+
+def total (num1,num2):
+    return num1 + num2 
+
+print(total(35,5))
+
+#3
+
+
+def smallest(num):
+    if not num:
+        return None  
+    return min(num)
+
+numbers_list = [3, -7, 0, 15, 2]
+print(smallest(numbers_list))
+
+# 4
+
+def pos(num):
+    return number > 0
+
+number = 5
+print(pos(number))
+# 5
+
+def sum(numbers):
+    return sum(numbers)
+print(sum(numbers_list))
